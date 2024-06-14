90bf88edbde732e3962b134da97e21fd3f0f74fd
Hongzzz <hongzzz@foxmail.com>
2024-06-15 02:03:50 +0800

    feat: decorator

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
+function log(originalMethod: Function, context: ClassMethodDecoratorContext) {
+  const methodName = String(context.name);
+  function replacementMethod(this: any, ...args: any[]) {
+    const className = String(this.constructor.name);
+    console.log(`LOG: Entering method '${className}.${methodName}'.`);
+    const result = originalMethod.call(this, ...args);
+    console.log(`LOG: Exiting method '${className}.${methodName}'.`);
+    return result;
+  }
+  return replacementMethod;
+}
+
 class Animal {
   name: string;
   constructor(aName: string) {
     this.name = aName;
   }
+  @log
   makeSound() {
     console.log(this.name, "make a sound");
   }
